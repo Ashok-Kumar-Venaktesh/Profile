e4de9e219509be81c4c1170b222c322339b29fa0
Ashok Kumar Venkatesh <28ash92@gmail.com>
2016-07-07 18:22:09 +0530

    Bootstrap Day1

--- Bootstrap/MyWebsite/includes/js/script.js
+++ Bootstrap/MyWebsite/includes/js/script.js
@@ -14,5 +14,14 @@ $(function(){
 		e.preventDefault();
 
 		$('#successAlert').slideDown();
-	})
+	});
+
+	$('a.pop').click(function(e){
+
+		e.preventDefault(); 
+	});
+
+	$('a.pop').popover();
+
+	$('[rel="tooltip"]').tooltip();
 })
